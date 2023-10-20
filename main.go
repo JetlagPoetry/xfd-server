be8dc6c0180bc368402d53501bd5302021ba1664
jingyuan <jingyuan_wang@cocafe.co>
2023-10-20 20:29:24 +0800

    test 4 gin routers

--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import (
 	"log"
 	"net/http"
 	"xfd-backend/database/db"
+	"xfd-backend/router"
 	"xfd-backend/service"
 )
 
 func main() {
 	Init()
 
-	//r := router.NewRouter()
+	r := router.NewRouter()
 	http.HandleFunc("/", service.IndexHandler)
 	http.HandleFunc("/api/count", service.CounterHandler)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatal(http.ListenAndServe(":80", r))
 }
 
 func Init() {
