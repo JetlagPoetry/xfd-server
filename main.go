28fea5e9cb41746b2e5336501e465a5c54836a7a
jingyuan <jingyuan_wang@cocafe.co>
2023-10-20 18:47:46 +0800

    test

--- main.go
+++ main.go
@@ -6,16 +6,16 @@ import (
 	"net/http"
 	"xfd-backend/config"
 	"xfd-backend/database/db"
-	"xfd-backend/router"
+	"xfd-backend/service"
 )
 
 func main() {
 	Init()
 
-	r := router.NewRouter()
-	//http.HandleFunc("/", service.IndexHandler)
-	//http.HandleFunc("/api/count", service.CounterHandler)
-	log.Fatal(http.ListenAndServe(":80", r))
+	//r := router.NewRouter()
+	http.HandleFunc("/", service.IndexHandler)
+	http.HandleFunc("/api/count", service.CounterHandler)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 func Init() {
