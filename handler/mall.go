a1acf4a6ace3da54915d3092b1dd7bab2b77c83a
GladiatorY <Gladiator_QY@outlook.com>
2023-11-02 15:50:49 +0800

    add cos

--- handler/mall.go
+++ handler/mall.go
@@ -1,12 +1,14 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 	"xfd-backend/database/db/model"
 	"xfd-backend/pkg/response"
 	"xfd-backend/pkg/types"
+	"xfd-backend/pkg/utils"
 	"xfd-backend/pkg/xerr"
 	"xfd-backend/service"
 )
@@ -17,10 +19,13 @@ type MallHandler struct {
 }
 
 func NewMallHandler() *MallHandler {
-	return &MallHandler{mallService: service.NewMallService(), areaService: service.NewAreaService()}
+	return &MallHandler{
+		mallService: service.NewMallService(),
+		areaService: service.NewAreaService(),
+	}
 }
 
-func (h MallHandler) GetCategories(c *gin.Context) {
+func (h *MallHandler) GetCategories(c *gin.Context) {
 	var (
 		req  types.CategoryListReq
 		resp []*model.Category
@@ -40,7 +45,7 @@ func (h MallHandler) GetCategories(c *gin.Context) {
 	c.JSON(http.StatusOK, response.RespSuccess(c, resp))
 }
 
-func (h MallHandler) GetArea(c *gin.Context) {
+func (h *MallHandler) GetArea(c *gin.Context) {
 	var (
 		req  types.AreaReq
 		resp []*types.AreaList
@@ -58,4 +63,23 @@ func (h MallHandler) GetArea(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, response.RespSuccess(c, resp))
+}
+
+func (h *MallHandler) UploadFile(c *gin.Context) {
+	file, header, err := c.Request.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusOK, fmt.Sprintf("上传文件失败: %s", err.Error()))
+		return
+	}
+	folderName := c.Request.FormValue("folderName")
+	if folderName == "" {
+		folderName = "temp"
+	}
+	link := utils.Upload(c, "xfd-t-1320959298", folderName+"/"+header.Filename, &file)
+	c.JSON(http.StatusOK, response.RespSuccess(c, link))
+}
+
+func (h *MallHandler) DeleteUploadFile(c *gin.Context) {
+	link := c.Query("link")
+	c.JSON(http.StatusOK, response.RespSuccess(c, utils.Delete(link)))
 }
