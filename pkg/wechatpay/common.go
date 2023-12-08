1ed5b65848c4dedd548cac6695fb025e7c495033
jingyuan <jingyuan@cocafe.co>
2023-12-08 21:14:21 +0800

    test

--- pkg/wechatpay/common.go
+++ pkg/wechatpay/common.go
@@ -2,67 +2,60 @@ package wechatpay
 
 import (
 	"context"
-	"crypto/tls"
-	"encoding/json"
-	"errors"
-	"fmt"
 	"github.com/wechatpay-apiv3/wechatpay-go/core"
 	"github.com/wechatpay-apiv3/wechatpay-go/services/payments"
 	"github.com/wechatpay-apiv3/wechatpay-go/services/payments/jsapi"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"time"
 	"xfd-backend/pkg/consts"
-	"xfd-backend/pkg/types"
 	"xfd-backend/pkg/utils"
 	"xfd-backend/pkg/xerr"
 )
 
-func GetWxOpenID(ctx context.Context, code string) (*types.WxOpenIDResp, xerr.XErr) {
-	var (
-		result *types.WxOpenIDResp
-		err    error
-	)
-	url := "https://api.weixin.qq.com/sns/jscode2session"
-	appID := os.Getenv("WECHAT_APP_ID")
-	secret := os.Getenv("WECHAT_APP_SECRET")
-	grantType := "authorization_code"
-
-	fullURL := fmt.Sprintf("%s?appid=%s&secret=%s&grant_type=%s&js_code=%s", url, appID, secret, grantType, code)
-
-	defer func() {
-		log.Printf("[WxService] GetWxOpenID called, url=%s, resp=%v, err=%v\n", fullURL, utils.ToJson(result), err)
-	}()
-
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-
-	client := &http.Client{Transport: tr}
-	resp, err := client.Get(fullURL)
-	if err != nil {
-		return nil, xerr.WithCode(xerr.ErrorCallApi, err)
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, xerr.WithCode(xerr.ErrorCallApi, err)
-	}
-
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		return nil, xerr.WithCode(xerr.ErrorCallApi, err)
-	}
-
-	if len(result.OpenID) == 0 {
-		log.Println("[GetWxOpenID] get openID failed, openID is empty")
-		return nil, xerr.WithCode(xerr.ErrorCallApi, errors.New("openid empty"))
-	}
-	return result, nil
-}
+//func GetWxOpenID(ctx context.Context, code string) (*types.WxOpenIDResp, xerr.XErr) {
+//	var (
+//		result *types.WxOpenIDResp
+//		err    error
+//	)
+//	url := "https://api.weixin.qq.com/sns/jscode2session"
+//	appID := os.Getenv("WECHAT_APP_ID")
+//	secret := os.Getenv("WECHAT_APP_SECRET")
+//	grantType := "authorization_code"
+//
+//	fullURL := fmt.Sprintf("%s?appid=%s&secret=%s&grant_type=%s&js_code=%s", url, appID, secret, grantType, code)
+//
+//	defer func() {
+//		log.Printf("[WxService] GetWxOpenID called, url=%s, resp=%v, err=%v\n", fullURL, utils.ToJson(result), err)
+//	}()
+//
+//	tr := &http.Transport{
+//		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+//	}
+//
+//	client := &http.Client{Transport: tr}
+//	resp, err := client.Get(fullURL)
+//	if err != nil {
+//		return nil, xerr.WithCode(xerr.ErrorCallApi, err)
+//	}
+//	defer resp.Body.Close()
+//
+//	body, err := ioutil.ReadAll(resp.Body)
+//	if err != nil {
+//		return nil, xerr.WithCode(xerr.ErrorCallApi, err)
+//	}
+//
+//	err = json.Unmarshal(body, &result)
+//	if err != nil {
+//		return nil, xerr.WithCode(xerr.ErrorCallApi, err)
+//	}
+//
+//	if len(result.OpenID) == 0 {
+//		log.Println("[GetWxOpenID] get openID failed, openID is empty")
+//		return nil, xerr.WithCode(xerr.ErrorCallApi, errors.New("openid empty"))
+//	}
+//	return result, nil
+//}
 
 func CreateOrder(ctx context.Context, orderSn, desc, openID string, price int64) (*jsapi.PrepayWithRequestPaymentResponse, xerr.XErr) {
 	var (
