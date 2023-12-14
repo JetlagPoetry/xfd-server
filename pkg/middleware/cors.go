ccc2341854f08a1e5cc47580e40e87b33ad80b5c
jingyuan <jingyuan@cocafe.co>
2023-12-14 16:44:28 +0800

    cors add headers

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -33,7 +33,7 @@ func Cors() gin.HandlerFunc {
 		// 服务器支持的所有跨域请求的方法,为了避免浏览次请求的多次'预检'请求
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
 		// header的类型
-		c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-ResourceType, Pragma, accessToken, X-AppId, X-Signature, X-Signature-Method")
+		c.Header("Access-Control-Allow-Headers", "Sec-CH-UA, Sec-CH-UA-Mobile, Sec-CH-UA-Platform, Referer, Content-Type, Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-ResourceType, Pragma, accessToken, X-AppId, X-Signature, X-Signature-Method")
 		// 允许跨域设置	 可以返回其他子段
 		// 跨域关键设置 让浏览器可以解析
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-ResourceType,Expires,Last-Modified,Pragma,FooBar")
