26e66444c5933f5b636e8d45558122b40812237e
Imogengu85 <50790596+Imogengu85@users.noreply.github.com>
2023-12-15 11:16:24 +0800

    add bind

--- pkg/types/order.go
+++ pkg/types/order.go
@@ -259,5 +259,5 @@ type GetCustomerServiceResp struct {
 }
 
 type ExportOrderReq struct {
-	QueryOrderIDs []int `json:"ids" form:"ids"`
+	QueryOrderIDs []int `json:"ids" form:"ids" binding:"required"`
 }
